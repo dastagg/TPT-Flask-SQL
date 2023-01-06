f3117e72e6e848a27566958c5413651706053860
staggsytems <staggsytems@staggsystems.com>
2023-01-06 12:43:11 -0500

    Added but commented out view_modifiers.

--- ch05_jinja_templates/pypi_org/app.py
+++ ch05_jinja_templates/pypi_org/app.py
@@ -1,4 +1,5 @@
 import flask
+# from pypi_org.infrastructure.view_modifiers import response
 
 app = flask.Flask(__name__)
 
@@ -14,13 +15,26 @@ def get_latest_packages():
 @app.route("/")
 def index():
     test_packages = get_latest_packages()
-
     return flask.render_template("home/index.html", packages=test_packages)
 
 
 @app.route("/about")
 def about():
     return flask.render_template("home/about.html")
+
+# @app.route("/")
+# @response(template_file='home/index.html')
+# def index():
+#     test_packages = get_latest_packages()
+#     return {'packages': test_packages}
+
+#     # return flask.render_template("home/index.html", packages=test_packages)
+
+
+# @app.route("/about")
+# @response(template_file='home/about.html')
+# def about():
+#     return {}
 
 
 if __name__ == "__main__":
